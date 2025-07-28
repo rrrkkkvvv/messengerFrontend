17a0bfd285ce4ec5550bfffa513629facde80bf7
Roman <136304611+rrrkkkvvv@users.noreply.github.com>
2025-07-29 01:25:47 +0200

    fix

--- src/entities/contact/ui/ContactsSkeleton.tsx
+++ src/entities/contact/ui/ContactsSkeleton.tsx
@@ -1,12 +1,12 @@
 const ContactsSkeleton = () => {
-  const skeletonContacts = Array(8).fill(null);
-
   return (
     <>
-      {skeletonContacts.map((_, index) => (
-        <div
-          key={index}
-          className="
+      {Array(8)
+        .fill(null)
+        .map((_, index) => (
+          <div
+            key={index}
+            className="
                     w-full
                     relative
                     flex
@@ -19,20 +19,20 @@ const ContactsSkeleton = () => {
                     border-green-200
                     p-2
                     animate-pulse"
-        >
-          <div className="h-9 w-9 rounded-full bg-gray-200 relative"></div>
+          >
+            <div className="h-9 w-9 rounded-full bg-gray-200 relative"></div>
 
-          <div>
-            <div className=" h-4  w-20 rounded-lg bg-gray-200"></div>
-            <div className=" flex   gap-2 relative text-center items-center mt-2">
-              <div className=" h-4  w-7 rounded-lg bg-gray-200"></div>
-
+            <div>
               <div className=" h-4  w-20 rounded-lg bg-gray-200"></div>
+              <div className=" flex   gap-2 relative text-center items-center mt-2">
+                <div className=" h-4  w-7 rounded-lg bg-gray-200"></div>
+
+                <div className=" h-4  w-20 rounded-lg bg-gray-200"></div>
+              </div>
             </div>
+            <div className="absolute right-5 top-2 h-4  w-16 rounded-lg bg-gray-200"></div>
           </div>
-          <div className="absolute right-5 top-2 h-4  w-16 rounded-lg bg-gray-200"></div>
-        </div>
-      ))}
+        ))}
     </>
   );
 };
