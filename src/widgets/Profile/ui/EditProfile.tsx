a369e9f33d3155d02b5c5aab268e60df4b41d5f7
Roman <136304611+rrrkkkvvv@users.noreply.github.com>
2025-10-08 11:40:17 +0200

    Update EditProfile.tsx

--- src/widgets/Profile/ui/EditProfile.tsx
+++ src/widgets/Profile/ui/EditProfile.tsx
@@ -168,7 +168,7 @@ const EditProfile = ({ currentUser, closeEditProfile }: TEditProfileProps) => {
               </button>
             )}
             {/* Username input */}
-            <Input onChange={handleInputChange} value={userName} type="input" />
+            <Input onChange={handleInputChange} value={userName} type="input" maxLength="30"/>
           </div>
           <SolidButton type="submit">Save and submit</SolidButton>
         </form>
