db4a90cc3d33f246409a00181ddfc6b389a4ca8c
Roman <136304611+rrrkkkvvv@users.noreply.github.com>
2025-11-14 01:18:01 +0100

    modified:   src/widgets/call/ui/callStates/ActiveCall.tsx

--- src/widgets/call/ui/callStates/ActiveCall.tsx
+++ src/widgets/call/ui/callStates/ActiveCall.tsx
@@ -71,7 +71,7 @@ const ActiveCall: FC<IActiveCallProps> = ({
       ) : (
         <div className="flex absolute w-full h-full z-10">
           <Video
-            className={`remote absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-full rounded-xl  object-contain `}
+            className={`remote absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-full   object-cover `}
             isMuted={false}
             stream={remoteStream}
             enabled={interlocuter.videoEnable}
